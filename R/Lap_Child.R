1e8780e8ef55c7ff3e23c7194d35edee78437250
Spgadou <spgadou@me.com>
2017-06-13 14:30:32 -0400

    Pas facile
    Correction mineure de Lap_Child

--- R/Lap_Child.R
+++ R/Lap_Child.R
@@ -11,16 +11,16 @@
 Lap_Child <- function(code, str, tt = "z")
 {
   str_ini <- str
-  lap <- stringr::str_replace_all(str@PGF, str@Param, str@Param)
+  lap <- stringr::str_replace_all(str@PGF, str@Param, str@parameter)
   for (i in 2:length(code))
   {
     code2 <- head(code, i)
     str2 <- Node(code2, str_ini)
 
     if (str2@type == "Mother")
-      ini <- stringr::str_replace_all(str2@PGF, str2@Param, str2@Param)
+      ini <- stringr::str_replace_all(str2@PGF, str2@Param, str2@parameter)
     else
-      ini <- stringr::str_replace_all(str2@Laplace, str2@Param, str2@Param)
+      ini <- stringr::str_replace_all(str2@Laplace, str2@Param, str2@parameter)
 
     lap <- stringr::str_replace_all(lap, "z", ini)
   }
