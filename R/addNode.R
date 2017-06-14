28d3f6cc2f0810a8c56e1746fa0907df46695810
Spgadou <spgadou@me.com>
2017-06-14 11:13:12 -0400

    addNode: Il y avait une twist
    -La fonction est maintenant fonctionnelle

--- R/addNode.R
+++ R/addNode.R
@@ -1,4 +1,36 @@
+#' Add a distribution
+#'
+#' @param type the type of the node (either 'Mother', 'Child' or 'Both')
+#' @param pp the parameter of the distribution used in the character strings
+#' @param name_short the short name of the distribution (ex.: 'log' for the logarithmic distribution)
+#' @param name_long the long name of the distribution (ex.: 'logarithmic' for the logarithmic distribution)
+#' @param Laplace the LST of the distribution (character), where
+#' @param LaplaceInv the inverse LST of the distribution (character)
+#' @param PGF the pgf of the distribution (character), if type is 'Mother' or 'Both'
+#' @param PGFInv the inverse pgf of the distribution (character), if type is 'Mother' or 'Both'
+#' @param simul function to sample from the distribution
+#' @param cop function to create a corresponding Archimedean copula class (ex.: for a GEO, it is an AMH copula), can be NULL
+#'
+#' @author Simon-Pierre Gadoury
+#'
+#' @examples
+#' addNode(type = "Child",
+#' pp = "gamma",
+#' name_short = "pois",
+#' name_long = "poisson",
+#' Laplace = "exp(gamma * (exp(-(z)) - 1))",
+#' LaplaceInv = "-log(log(z) / gamma + 1)",
+#' NULL,
+#' NULL,
+#' simul = function(n, gamma) rpois(n, gamma),
+#' NULL)
+#'
+#' POIS(5, 1:2, NULL)
+#'
+#' @export
+
 addNode <- function(type,
+                    pp,
                     name_short,
                     name_long,
                     Laplace,
@@ -12,11 +44,17 @@ addNode <- function(type,
     stop("The type should be either 'Child' or 'Mother'")
   else
   {
+
+    name_long <- tolower(name_long)
+    char1 <- strsplit(name_long, "")[[1]]
+    char1[1] <- toupper(char1[1])
+    name_long <- paste(char1, collapse = "")
+
     ## Class name
     name_short <- tolower(name_short)
     char1 <- strsplit(name_short, "")[[1]]
     char1[1] <- toupper(char1[1])
-    char1 <- paste(char1, sep = "")
+    char1 <- paste(char1, collapse = "")
     name_class <- paste(char1, "_", type, sep = "")
 
     ## Create the class
@@ -37,8 +75,8 @@ addNode <- function(type,
                   theta = "numeric",
                   LTheta = "character",
                   cop = "function"),
-              contains = type)
-    else
+              contains = type, where = .GlobalEnv)
+    else if (type == "Mother")
       setClass(name_class,
                list(name = "character",
                     type = "character",
@@ -56,7 +94,59 @@ addNode <- function(type,
                     theta = "numeric",
                     PM = "character",
                     cop = "function"),
-               contains = type)
+               contains = type, where = .GlobalEnv)
+    else if (type == "Both")
+    {
+      type <- "Mother"
+      name_short <- tolower(name_short)
+      char1 <- strsplit(name_short, "")[[1]]
+      char1[1] <- toupper(char1[1])
+      char1 <- paste(char1, collapse = "")
+      name_class <- paste(char1, "_", type, sep = "")
+
+      setClass(name_class,
+               list(name = "character",
+                    type = "character",
+                    dimension = "numeric",
+                    parameter = "numeric",
+                    arg = "numeric",
+                    structure = "list",
+                    obj = "character",
+                    Param = "character",
+                    Laplace = "character",
+                    LaplaceInv = "character",
+                    PGF = "character",
+                    PGFInv = "character",
+                    simul = "function",
+                    theta = "numeric",
+                    PM = "character",
+                    cop = "function"),
+               contains = type, where = .GlobalEnv)
+
+      type <- "Child"
+      name_short <- tolower(name_short)
+      char1 <- strsplit(name_short, "")[[1]]
+      char1[1] <- toupper(char1[1])
+      char1 <- paste(char1, collapse = "")
+      name_class <- paste(char1, "_", type, sep = "")
+      setClass(name_class,
+               list(name = "character",
+                    type = "character",
+                    dimension = "numeric",
+                    parameter = "numeric",
+                    arg = "numeric",
+                    obj = "character",
+                    Param = "character",
+                    Laplace = "character",
+                    LaplaceInv = "character",
+                    PGF = "character",
+                    PGFInv = "character",
+                    simul = "function",
+                    theta = "numeric",
+                    LTheta = "character",
+                    cop = "function"),
+               contains = type, where = .GlobalEnv)
+    }
 
     FF <- compiler::cmpfun(function(par, unif, struc)
     {
@@ -82,20 +172,24 @@ addNode <- function(type,
       if (t@type == "Mother")
       {
         t@Param <- "gamma"
-        t@Laplace <- Laplace
-        t@LaplaceInv <- LaplaceInv
-        t@PGF <- PGF
-        t@PGFInv <- PGFInv
+        t@Laplace <- stringr::str_replace_all(Laplace, pp, "gamma")
+        t@LaplaceInv <- stringr::str_replace_all(LaplaceInv, pp, "gamma")
+        t@PGF <- stringr::str_replace_all(PGF, pp, "gamma")
+        t@PGFInv <- stringr::str_replace_all(PGFInv, pp, "gamma")
       }
       else
       {
         t@Param <- "alpha"
-        t@Laplace <- Laplace
-        t@LaplaceInv <- LaplaceInv
+        t@Laplace <- stringr::str_replace_all(Laplace, pp, "alpha")
+        t@LaplaceInv <- stringr::str_replace_all(LaplaceInv, pp, "alpha")
       }
       t@simul <- simul
       t@theta <- vector("numeric")
-      t@cop <- cop
+
+      if (is.null(cop))
+        t@cop <- function() "No known Archimedean copula related to this distribution"
+      else
+        t@cop <- cop
 
       t
     })
