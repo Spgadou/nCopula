252fe01874afecef5f7256bbd5682520773809ee
David Beauchemin <freresdarme@hotmail.com>
2017-08-01 17:22:56 -0400

    add density frank copulas

--- R/Frank_Copulas.R
+++ R/Frank_Copulas.R
@@ -1,10 +1,10 @@
 #' Construction of an Archimedean Copula Class Object
 #'
-#' Constructs Frank Archimedean copula object with a given parameter and dimension
+#' Constructs Frank Archimedean copula object with a given parameter and dimension.
 #'
-#' @param dim Dimension of the copula (>= 2), which is, by default, 2
-#' @param param Parameter of the copula
-#' @param density Should the expression of the density be computed ?
+#' @param dim Dimension of the copula (>= 2), which is, by default, 2.
+#' @param param Parameter of the copula.
+#' @param density Compute the expression of the density of the copulas.
 #'
 #' @author Simon-Pierre Gadoury
 #'
@@ -38,4 +38,46 @@ Frank <- compiler::cmpfun(function(param, dim = 2L)
       dimension = dim,
       parameter = param,
       name = "Frank copula")
+  if (density)
+  {
+       tt <- GAMMA(1/10, 1:dim, NULL)
+       
+       uu <- paste("u", 1:dim, sep = "")
+       expr1 <- numeric(dim)
+       for (i in 1:dim)
+            expr1[i] <- stringr::str_replace_all(tt@Der("z", 1, "LaplaceInv"), "z", uu[i])
+       expr1 <- paste("(", expr1, ")", sep = "", collapse = " * ")
+       
+       nu <- numeric(dim)
+       for(i in 1:dim)
+            nu[i] <- stringr::str_replace_all(tt@LaplaceInv, "z", uu[i])
+       nu <- paste("(", nu, ")", sep = "", collapse = " + ")
+       
+       expr2 <- stringr::str_replace_all(tt@Der("z", dim, "Laplace"), "z", nu)
+       densit <- paste("(", expr1, ") * (", expr2, ")", sep = "")
+       densit <- stringr::str_replace_all(densit, "alpha", "(1/alpha)")
+       
+       new("frank",
+           phi = phi,
+           phi.inv = phi.inv,
+           rBiv = rBiv,
+           theta = th,
+           depend = dep.param,
+           dimension = dim,
+           parameter = param,
+           dens = densit,
+           name = "Frank copula")
+  }
+  else
+  {
+       new("frank",
+           phi = phi,
+           phi.inv = phi.inv,
+           rBiv = rBiv,
+           theta = th,
+           depend = dep.param,
+           dimension = dim,
+           parameter = param,
+           name = "Frank copula")
+  }
 })
