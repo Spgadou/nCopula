b6e349cd583f0cc3b4488a9cb0099b827d5b8fdd
Spgadou <spgadou@me.com>
2017-08-01 15:19:48 -0400

    Archives
    rCompCop2 -> rCompCop

--- R/rCompCop.R
+++ R/rCompCop.R
@@ -1,175 +1,119 @@
 #' Density, Cdf, and Random Number Generator for Copulas Constructed Through Compounding
 #'
-#' @description The density, the CDF, and the random number generator for copulas construted
-#' through compounding.
+#' @description The function rCompCop() is a random number generator for copulas constructed
+#'  through compounding. It sample from a know structure (S4 class of type 'Mother')
 #' @param n Number of realisations
-#' @param FUN Object of class Mother
-#' @param level Number of imbrications
+#' @param str Object of class Mother
 #'
 #' @details rCompCop2 is more general (and easier to use) than rCompCop, but is slower.
 #'
-#' @author Simon-Pierre Gadoury
+#' @return ...
 #'
+#' @examples
+#' str <- GEO(0.1, 1, list(GAMMA(0.2, 2:3, NULL),
+#'                         GEO(0.3, 4:5, NULL)))
+#' rCompComp(1000, str)
+#'
+#' @author Simon-Pierre Gadoury
 #' @export
 
-rCompCop <- compiler::cmpfun(function(n, FUN, level){
+rCompCop <- compiler::cmpfun(function(n, str)
+{
+  e1 <- new.env()
+  e1$res <- list()
+  gen <- GeneticCodes(str)
+  e1$M0 <- str@simul(n, str@parameter)
 
-  if (FUN@type == "Child")
+  for (i in 1:length(gen))
   {
-    FUN@theta <- FUN@simul(n, FUN@parameter)
-    laplace <- stringr::str_replace_all(FUN@Laplace, FUN@Param, FUN@parameter)
-    th <- "-log(runif(FUN@dimension * n)) / FUN@theta"
-    laplace <- parse(text = stringr::str_replace_all(laplace, "z", th))
-    return(matrix(eval(laplace), ncol = FUN@dimension, nrow = n))
-  }
-
-  t <- FUN
-  res <- list()
-  FUN <- list()
-
-  th.pos1 <- list()
-  m.pos1 <- list()
-
-  th.pos1[[1]] <- list()
-  m.pos1[[1]] <- list()
-
-  FUN[[1]] <- list()
-  FUN[[1]][[1]] <- t
-
-
-  for (i in 1:level)
-  {
-    FUN[[i + 1]] <- list()
-
-    if (i == 1){
-
-      FUN[[1]][[1]]@theta <- FUN[[1]][[1]]@simul(n, FUN[[1]][[1]]@parameter)
-
-      FUN[[1]][[1]]@PGF <- stringr::str_replace_all(FUN[[1]][[1]]@PGF, FUN[[1]][[1]]@Param, FUN[[1]][[1]]@parameter)
-
-      if (FUN[[1]][[1]]@type == "Mother"){
-        typ <- numeric(FUN[[1]][[1]]@dimension)
-        for (j in 1:length(FUN[[1]][[1]]@structure))
-          typ[j] <- FUN[[1]][[1]]@structure[[j]]@type}
-
-      th.pos1[[1]][[1]] <- which(typ == "Child")
-      m.pos1[[1]][[1]] <- which(typ == "Mother")
-
-      if (length(FUN[[1]][[1]]@arg) > 1 || {length(FUN[[1]][[1]]@arg) == 1 && FUN[[1]][[1]]@arg != 0})
-      {
-        for (j in 1:length(FUN[[1]][[1]]@arg))
-        {
-          laplace <- stringr::str_replace_all(FUN[[1]][[1]]@Laplace, FUN[[1]][[1]]@Param, FUN[[1]][[1]]@parameter)
-          th <- "-log(runif(n)) / FUN[[1]][[1]]@theta"
-          laplace <- parse(text = stringr::str_replace_all(laplace, "z", th))
-          res[[FUN[[1]][[1]]@arg[j]]] <- eval(laplace)
-        }
-      }
-
-      if (length(th.pos1[[1]][[1]]) != 0){
-        for (j in 1:length(th.pos1[[1]][[1]]))
-        {
-          laplace <- stringr::str_replace_all(FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@Laplace, FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@Param, FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@parameter)
-          fbarre <- stringr::str_replace_all(FUN[[1]][[1]]@PGF, "z", laplace)
-
-          argg <- FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@arg
-
-          th2 <- vapply(FUN[[1]][[1]]@theta, function(x) sum(FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@simul(x, FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@parameter)), rep(0, 1))
-
-          res2 <- list()
-          for (l in 1:length(argg))
-          {
-            th <- -log(runif(n)) / th2
-
-            fbarre <- stringr::str_replace_all(fbarre, "z", "th")
-
-            res[[argg[l]]] <- eval(parse(text = fbarre))
-          }
-        }}
-
-      if (length(m.pos1[[1]][[1]]) != 0){
-        for (j in 1:length(m.pos1[[1]][[1]]))
-        {
-
-          FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@PGF <- stringr::str_replace_all(FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@PGF, FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@Param, FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@parameter)
-
-          FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@PGF <- stringr::str_replace_all(FUN[[1]][[1]]@PGF, "z", FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@PGF)
-
-          FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@theta <- vapply(FUN[[1]][[1]]@theta, function(x) sum(FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@simul(x, FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]@parameter)), rep(0, 1))
-
-          FUN[[i + 1]][[j]] <- FUN[[1]][[1]]@structure[[m.pos1[[1]][[1]][j]]]
-        }}
-
-    }
-
-    else
+    ## Ce bloc la est good je pense
+    if (length(gen[[i]]) == 2)
     {
-      th.pos1[[i]] <- list()
-      m.pos1[[i]] <- list()
+      str2 <- Node(gen[[i]], str)
+      M.prec <- eval(parse(text = paste("e1$M", paste(gen[[i]][1], collapse = ""), sep = "")))
 
-      for (k in 1:length(m.pos1[[i - 1]][[1]]))
+      R <- matrix(rexp(length(str2@arg) * n, 1), ncol = length(str2@arg), nrow = n)
+
+      if (gen[[i]][length(gen[[i]])] == 0)
       {
+        Theta <- M.prec
 
-        typ <- numeric(FUN[[i]][[k]]@dimension)
-        for (j in 1:length(FUN[[i]][[k]]@structure))
-          typ[j] <- FUN[[i]][[k]]@structure[[j]]@type
+        ini <- stringr::str_replace_all(str@Laplace, str@Param, str@parameter)
+        ff <- function(z) eval(parse(text = ini))
+        e1$res[[i]] <- ff(R / Theta)
+      }
+      else
+      {
+        Theta <- matrix(rep(vapply(1:length(M.prec), function(t) sum(str2@simul(M.prec[t], str2@parameter)), 0), length(str2@arg)),
+                        ncol = length(str2@arg), nrow = n)
 
-        th.pos1[[i]][[k]] <- which(typ == "Child")
-        m.pos1[[i]][[k]] <- which(typ == "Mother")
+        ini <- stringr::str_replace_all(str@PGF, str@Param, str@parameter)
+        ini <- stringr::str_replace_all(ini, "z",
+                                        stringr::str_replace_all(str2@Laplace, str2@Param, str2@parameter))
+        ff <- function(z) eval(parse(text = ini))
+        e1$res[[i]] <- ff(R / Theta)
+      }
+    }
+    else if (length(gen[[i]]) > 2)
+    {
+      ## Initialiser la Laplace du Theta final
+      Lap <- stringr::str_replace_all(str@PGF, str@Param, str@parameter)
 
-        if (FUN[[i]][[k]]@arg != 0)
+      for (j in 2:(length(gen[[i]]) - 1))
+      {
+        ## Sous structure
+        str2 <- Node(gen[[i]][1:j], str)
+
+        ## Conditions pour bâtir la Laplace
+        if (gen[[i]][length(gen[[i]])] != 0)
         {
-          for (j in 1:length(FUN[[i]][[k]]@arg))
+          ini <- stringr::str_replace_all(str2@PGF, str2@Param, str2@parameter)
+          Lap <- stringr::str_replace_all(Lap, "z", ini)
+        }
+        else
+        {
+          if (j == length(gen[[i]]) - 1)
           {
-            laplace <- stringr::str_replace_all(FUN[[i]][[k]]@Laplace, FUN[[i]][[k]]@Param, FUN[[i]][[k]]@parameter)
-            th <- "-log(runif(n)) / FUN[[i]][[k]]@theta"
-            laplace <- parse(text = stringr::str_replace_all(laplace, "z", th))
-            res[[FUN[[i]][[k]]@arg[j]]] <- eval(laplace)
+            ini <- stringr::str_replace_all(str2@Laplace, str2@Param, str2@parameter)
+            Lap <- stringr::str_replace_all(Lap, "z", ini)
+          }
+          else
+          {
+            ini <- stringr::str_replace_all(str2@PGF, str2@Param, str2@parameter)
+            Lap <- stringr::str_replace_all(Lap, "z", ini)
           }
         }
 
-        if (length(th.pos1[[i]][[k]]) != 0){
-          for (j in 1:length(th.pos1[[i]][[k]]))
-          {
-            laplace <- stringr::str_replace_all(FUN[[i]][[k]]@structure[[th.pos1[[i]][[k]][j]]]@Laplace, FUN[[i]][[k]]@structure[[th.pos1[[i]][[k]][j]]]@Param, FUN[[i]][[k]]@structure[[th.pos1[[i]][[k]][j]]]@parameter)
+        variable0 <- paste("M", paste(gen[[i]][1:(j - 1)], collapse = ""), sep = "") ## Le M au dessus du M
+        variable1 <- paste("M", paste(gen[[i]][1:j], collapse = ""), sep = "") ## Le M
 
-            fbarre <- stringr::str_replace_all(FUN[[i]][[k]]@PGF, "z", laplace)
-
-            argg <- FUN[[i]][[k]]@structure[[th.pos1[[i]][[1]][j]]]@arg
-
-            th2 <- vapply(FUN[[i]][[k]]@theta, function(x) sum(FUN[[i]][[k]]@structure[[th.pos1[[i]][[k]][j]]]@simul(x, FUN[[i]][[k]]@structure[[th.pos1[[i]][[k]][j]]]@parameter)), rep(0, 1))
-
-            res2 <- list()
-            for (l in 1:length(argg))
-            {
-              th <- -log(runif(n)) / th2
-
-              fbarre <- parse(text = stringr::str_replace_all(fbarre, "z", "th"))
-
-              res[[argg[l]]] <- eval(fbarre)
-            }
-
-            #res[[argg[1]]] <- matrix(unlist(res2), ncol = FUN[[1]][[1]]@structure[[th.pos1[[1]][[1]][j]]]@dimension, nrow = n)
-          }}
-
-        if (length(m.pos1[[i]][[k]]) != 0){
-          for (j in 1:length(m.pos1[[i]][[k]]))
-          {
-
-            FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@PGF <- stringr::str_replace_all(FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@PGF,
-                                                                                           FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@Param,
-                                                                                           FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@parameter)
-
-            FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@PGF <- stringr::str_replace_all(FUN[[i]][[k]]@PGF, "z", FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@PGF)
-
-            FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@theta <- vapply(FUN[[i]][[k]]@theta, function(x) sum(FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@simul(x, FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]@parameter)), rep(0, 1))
-
-            FUN[[i + 1]][[j]] <- FUN[[i]][[k]]@structure[[m.pos1[[i]][[k]][j]]]
-
-          }}
+        if (!exists(variable1, envir = e1))
+        {
+          ## C'est important de bien le sentir
+          eval(parse(text = paste("e1$", variable1, " <- vapply(1:length(", paste("e1$", variable0, sep = ""), "),
+                                  function(t) sum(str2@simul(", paste("e1$", variable0, "[t],", sep = ""), "str2@parameter)), 0)", sep = "")))
+        }
       }
-    }
 
+      str2 <- Node(gen[[i]], str)
+      M.prec <- eval(parse(text = paste("e1$M", paste(gen[[i]][1:(length(gen[[i]]) - 1)], collapse = ""), sep = "")))
+
+      if (gen[[i]][length(gen[[i]])] != 0)
+      {
+        ini <- stringr::str_replace_all(str2@Laplace, str2@Param, str2@parameter)
+        Lap <- stringr::str_replace_all(Lap, "z", ini)
+
+        Theta <- matrix(rep(vapply(1:length(M.prec), function(t) sum(str2@simul(M.prec[t], str2@parameter)), 0), length(str2@arg)),
+                        ncol = length(str2@arg), nrow = n)
+      }
+      else
+        Theta <- M.prec
+
+      R <- matrix(rexp(length(str2@arg) * n, 1), ncol = length(str2@arg), nrow = n)
+
+      ff <- function(z) eval(parse(text = Lap))
+      e1$res[[i]] <- ff(R / Theta)
+    }
   }
-  do.call(cbind, res)
+  do.call(cbind, e1$res)
 })
